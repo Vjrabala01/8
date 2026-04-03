f64d25d09cc6587b1337001540ba832f4e717a6f
Vjrabala01 <128447170+Vjrabala01@users.noreply.github.com>
2026-04-03 18:18:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
-// Ito ang nagpapatakbo sa NYXEN App sa background
+// sw.js - Simple Version for NYXEN
 self.addEventListener('install', (e) => {
-  console.log('NYXEN Service Worker Installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  // Pinapayagan nito ang app na mag-load ng mabilis
   e.respondWith(fetch(e.request));
 });
